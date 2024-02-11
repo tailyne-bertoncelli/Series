88c6132d47c9aec884a322fb535076b2939245e2
Tailyne Bertoncelli <tailyne.berton@gmail.com>
2024-02-11 00:45:55 -0300

    commit

--- resources/views/series/edit.blade.php
+++ resources/views/series/edit.blade.php
@@ -1,5 +1,3 @@
 <x-layout title="Editar series '{{$serie->nome}}'">
-    <x-series.form action="{{route('series.update', $serie->id)}}"
-                   nome="{{$serie->nome}}"
-    ></x-series.form>
+    <x-series.form action="{{route('series.update', $serie->id)}}" nome="{{$serie->nome}}"></x-series.form>
 </x-layout>
